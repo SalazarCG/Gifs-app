30e5329843d746ff6455d458e4bf6ec2f9f0f6b7
Cristian <389336@alu.murciaeduca.es>
2024-10-13 13:23:24 +0200

    CardListAndShearchBox

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home/home-pagecomponent';
+import { SearchBoxComponent } from './components/search-box/shearch-box.component';
+import { CardListComponent } from './pages/card-list/card-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomePageComponent,
   ]
 })
 export class GifsModule { }
