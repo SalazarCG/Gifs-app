44b72c6cb6fbc53c2b8383a965fd02d218291dbe
Cristian <389336@alu.murciaeduca.es>
2024-10-16 09:54:05 +0200

    Fin de la sección. Se añade la búsqueda de los gifs, su persistencia en local el load y la autocarga del historial de gifs sobre la posicion 0

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,19 +1,27 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
-  private _tagsHistory: String[] = [];
 
-  private apiKey: String = 'YZxeT3T2CEeNModMo4oQqFIwS9OyZdfe';
+  public gifList: Gif[] = [];
 
-  constructor(private http: HttpClient) {}
+  private _tagsHistory: string[] = [];
+  private apiKey:       string = "YZxeT3T2CEeNModMo4oQqFIwS9OyZdfe";
+  private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
+
+
 
   private organizeHistory(tag: string) {
     tag = tag.toLowerCase();
@@ -26,18 +34,38 @@ export class GifsService {
 
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this.tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
   }
 
-  searchTag(tag: string):void {
+  //grabar en local storage
+  private saveLocalStorage():void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return
+
+    this._tagsHistory= JSON.parse(localStorage.getItem('history')! ) ;
+
+    if (this._tagsHistory.length ===0) return;
+    this.searchTag(this._tagsHistory[0]);
+  }
+
+  searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
 
-    this.http.get(
-      'https://api.giphy.com/v1/gifs/search?api_key=YZxeT3T2CEeNModMo4oQqFIwS9OyZdfe&q=valorant&limit=10'
-    ).subscribe( resp => {
-      console.log(resp);
-    } );
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag)
 
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
+        //console.log({gifs: this.gifList});
+      });
   }
 }
 
