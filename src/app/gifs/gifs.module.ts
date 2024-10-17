67b9e50420b58ac96b39bee9b25cbb98bec111a4
Cristian <389336@alu.murciaeduca.es>
2024-10-17 10:03:20 +0200

    Fin del proyecto Gifs

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+
 import { HomePageComponent } from './pages/home/home-pagecomponent';
 import { SearchBoxComponent } from './components/search-box/shearch-box.component';
-import { CardListComponent } from './pages/card-list/card-list.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
+
 
 
 
@@ -11,9 +16,11 @@ import { CardListComponent } from './pages/card-list/card-list.component';
     HomePageComponent,
     SearchBoxComponent,
     CardListComponent,
+    CardComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent,
